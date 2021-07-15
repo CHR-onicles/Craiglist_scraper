09b7a27ad0c51fbf7adcbd92611ef4ac712cac56
CHR-onicles <tpandivine48@gmail.com>
2021-07-15 17:51:11 +0000

    Delete script.js

--- craig_app/static/craig_app/script.js
+++
@@ -1,9 +0,0 @@
-let card_titles = document.querySelectorAll(".card-content p");
-
-for (let index = 0; index < card_titles.length; index++) {
-    if (String(card_titles[index].length > 20)){
-        card_titles[index].innerText = card_titles[index].innerText.strip().slice(0, 17) + '...';
-    }
-}
-
-//not working - cant load JS properly
